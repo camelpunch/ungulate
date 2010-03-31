78adf0ab1f7dc6cb7523a018ed2ca6af15bdf5c8
Andrew Bruce <andrew@camelpunch.com>
2010-03-31 16:14:20 +0100

    some spec stubbing

---
+++ lib/ungulate.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+require 'right_aws'
+module Ungulate
+end
--- spec/ungulate_spec.rb
+++ spec/ungulate_spec.rb
@@ -1,7 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Ungulate" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Ungulate do
+  describe "run" do
+    before do
+      ENV['AMAZON_ACCESS_KEY_ID'] = 'test-secret'
+      ENV['AMAZON_SECRET_ACCESS_KEY'] = 'test-key-id'
+      ENV['QUEUE'] = 'test-queue'
+
+      @instruction = '{
+        bucket: test-bucket, 
+        key: test-key, 
+        versions: {
+          thumb: { width: 100, height: 200 },
+          large: { width: 200, height: 300 }
+        }
+      }'
+
+      @q = mock('Queue')
+      @q.stub!(:pop).and_return(@instruction)
+      @sqs = mock('SqsGen2')
+      @sqs.stub!(:queue).with('test-queue').and_return(@q)
+      RightAws::SqsGen2.stub!(:new).with('test-key-id', 'test-secret').and_return(@sqs)
+    end
+
+    context "with thumbnail and large versions" do
+      it "should send a thumbnail to S3"
+      it "should sened a large version to S3"
+    end
   end
 end
