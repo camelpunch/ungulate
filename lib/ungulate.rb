43a43881beca2708483c3705d331b47e81998200
Andrew Bruce <andrew@camelpunch.com>
2010-04-02 11:17:28 +0100

    pop from queue using environment variables

--- lib/ungulate.rb
+++ lib/ungulate.rb
@@ -1,4 +1,32 @@
 require 'rubygems'
 require 'right_aws'
 module Ungulate
+  class Runner
+    def run
+      job = Job.pop
+    end
+
+    def pop_job
+    end
+  end
+
+  class Job
+    attr_accessor :bucket, :key
+
+    def self.pop
+      job = new
+      message = queue.pop
+      job_attributes = YAML.load message
+      job.bucket = job_attributes[:bucket]
+      job.key = job_attributes[:key]
+
+      job
+    end
+
+    def self.queue
+      @queue ||= RightAws::SqsGen2.new(ENV['AMAZON_ACCESS_KEY_ID'],
+                                       ENV['AMAZON_SECRET_ACCESS_KEY']).
+                                       queue(ENV['QUEUE'])
+    end
+  end
 end
--- spec/ungulate_spec.rb
+++ spec/ungulate_spec.rb
@@ -1,31 +1,74 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe Ungulate do
-  describe "run" do
-    before do
-      ENV['AMAZON_ACCESS_KEY_ID'] = 'test-secret'
-      ENV['AMAZON_SECRET_ACCESS_KEY'] = 'test-key-id'
-      ENV['QUEUE'] = 'test-queue'
-
-      @instruction = '{
-        bucket: test-bucket, 
-        key: test-key, 
-        versions: {
-          thumb: { width: 100, height: 200 },
-          large: { width: 200, height: 300 }
+module Ungulate
+  describe Job do
+    describe :pop do
+      before do
+        @job_attributes = {
+          :bucket => 'test-bucket', 
+          :key => 'test-key', 
+          :versions => {
+            :thumb => [ :resize_to_fill, 100, 200 ],
+            :large => [ :resize_to_fill, 200, 300 ],
+          }
         }
-      }'
 
-      @q = mock('Queue')
-      @q.stub!(:pop).and_return(@instruction)
-      @sqs = mock('SqsGen2')
-      @sqs.stub!(:queue).with('test-queue').and_return(@q)
-      RightAws::SqsGen2.stub!(:new).with('test-key-id', 'test-secret').and_return(@sqs)
+        @message = mock('Message', :read => @job_attributes.to_yaml)
+        @q = mock('Queue')
+        @q.stub(:pop).and_return(@message)
+        Job.stub(:queue).and_return(@q)
+      end
+
+      subject { Job.pop }
+
+      it { should be_a(Job) }
+      its(:bucket) { should == @job_attributes[:bucket] }
+      its(:key) { should == @job_attributes[:key] }
     end
 
-    context "with thumbnail and large versions" do
-      it "should send a thumbnail to S3"
-      it "should sened a large version to S3"
+    describe :queue do
+      before do
+        ENV['AMAZON_ACCESS_KEY_ID'] = 'test-key-id'
+        ENV['AMAZON_SECRET_ACCESS_KEY'] = 'test-secret'
+        ENV['QUEUE'] = 'test-queue'
+
+        @q = mock('Queue')
+        @sqs = mock('Sqs')
+        @sqs.stub(:queue).with('test-queue').and_return(@q)
+
+        RightAws::SqsGen2.stub(:new).with('test-key-id', 'test-secret').and_return(@sqs)
+      end
+
+      subject { Job.queue }
+
+      it { should == @q }
+
+      describe "when already called" do
+        before do
+          Job.instance_variable_set('@queue', @q)
+        end
+
+        it "should not instantiate afresh" do
+          RightAws::SqsGen2.should_not_receive(:new)
+          Job.queue
+        end
+      end
+    end
+  end
+
+  describe Runner do
+    before do
+      @job = mock('Ungulate::Job')
+
+      Ungulate::Job.stub(:pop).and_return(@job)
+    end
+
+    describe "run" do
+      after { subject.run }
+
+      it "should pop a job" do
+        Ungulate::Job.should_receive(:pop)
+      end
     end
   end
 end
