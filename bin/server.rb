ac2db13c87fe8fb381d34e52d58aa8c1f349c114
Andrew Bruce <andrew@camelpunch.com>
2010-04-06 00:02:55 +0100

    server copes with exceptions above StandardError, exits if not given queue name

--- bin/server.rb
+++ bin/server.rb
@@ -2,4 +2,18 @@
 
 require 'lib/ungulate'
 
-loop { Ungulate.run(ARGV[0]) }
+if ARGV[0].nil?
+  $stderr.puts "Must provide a queue name after calling the server"
+  exit 1
+end
+
+logger = Logger.new STDERR
+
+loop do
+  begin
+    sleep 1
+    Ungulate.run(ARGV[0]) 
+  rescue StandardError => e
+    logger.error e.message
+  end
+end
