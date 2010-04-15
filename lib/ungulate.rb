b36c6b082e5c7a7bf9496209d077eae87d633f3f
Andrew Bruce <andrew@camelpunch.com>
2010-04-15 20:14:03 +0100

    inject ungulate view helpers when Rails found

--- lib/ungulate.rb
+++ lib/ungulate.rb
@@ -3,3 +3,9 @@ require 'ungulate/server'
 
 module Ungulate
 end
+
+if defined? Rails
+  return if ActionView::Base.instance_methods.include_method? :ungulate_upload_form_for
+  require 'ungulate/view_helpers'
+  ActionView::Base.send :include, ViewHelpers
+end
