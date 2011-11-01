518a29867a1b59cdf7ba089fba932b1e19f4f9b6
Marco Antonio <marcofognog@gmail.com>
2011-11-01 17:00:30 -0200

    FileUpload was not beeing loaded.

--- lib/ungulate/file_upload.rb
+++ lib/ungulate/file_upload.rb
@@ -1,75 +1,77 @@
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/json'
 
-class Ungulate::FileUpload
-  attr_accessor :bucket_url, :key
+module Ungulate
+  class FileUpload
+    attr_accessor :bucket_url, :key
 
-  class << self
+    class << self
+      def config
+        Ungulate.configuration
+      end
+
+      def queue
+        @queue ||= config.queue.call
+      end
+
+      def enqueue(job_description)
+        queue.push(job_description.to_yaml)
+      end
+    end
+
+    def initialize(options = {})
+      self.bucket_url = options[:bucket_url]
+      self.key = options[:key]
+
+      if options[:policy]
+        self.policy = options[:policy]
+      end
+    end
+
     def config
-      Ungulate.configuration
+      self.class.config
     end
 
-    def queue
-      @queue ||= config.queue.call
+    def access_key_id
+      config.access_key_id
     end
 
-    def enqueue(job_description)
-      queue.push(job_description.to_yaml)
+    def acl
+      condition 'acl'
     end
-  end
 
-  def initialize(options = {})
-    self.bucket_url = options[:bucket_url]
-    self.key = options[:key]
-
-    if options[:policy]
-      self.policy = options[:policy]
+    def condition(key)
+      found_key, found_value = conditions.find {|condition| condition.first == key}
+      found_value if found_value
     end
-  end
 
-  def config
-    self.class.config
-  end
+    def conditions
+      @conditions ||=
+        @policy_ruby['conditions'].map {|condition| condition.to_a.flatten}
+    end
 
-  def access_key_id
-    config.access_key_id
-  end
+    def policy
+      Base64.encode64(
+        ActiveSupport::JSON.encode(@policy_ruby)
+      ).gsub("\n", '')
+    end
 
-  def acl
-    condition 'acl'
-  end
+    def policy=(new_policy)
+      new_policy['expiration'] = new_policy['expiration'].utc
+      @policy_ruby = new_policy
+      policy
+    end
 
-  def condition(key)
-    found_key, found_value = conditions.find {|condition| condition.first == key}
-    found_value if found_value
-  end
+    def success_action_redirect
+      condition 'success_action_redirect'
+    end
 
-  def conditions
-    @conditions ||=
-      @policy_ruby['conditions'].map {|condition| condition.to_a.flatten}
-  end
-
-  def policy
-    Base64.encode64(
-      ActiveSupport::JSON.encode(@policy_ruby)
-    ).gsub("\n", '')
-  end
-
-  def policy=(new_policy)
-    new_policy['expiration'] = new_policy['expiration'].utc
-    @policy_ruby = new_policy
-    policy
-  end
-
-  def success_action_redirect
-    condition 'success_action_redirect'
-  end
-
-  def signature
-    Base64.encode64(
-      OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'),
-                           config.secret_access_key,
-                           policy)
-    ).gsub("\n", '')
+    def signature
+      Base64.encode64(
+        OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'),
+                             config.secret_access_key,
+                             policy)
+      ).gsub("\n", '')
+    end
   end
 end
