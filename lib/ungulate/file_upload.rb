70ebe00608ccb79dba2713b0911279a0db96c8ee
Marco Antonio <marcofognog@gmail.com>
2011-11-07 12:32:12 -0200

    Need to rquire FileUpload.

--- lib/ungulate/file_upload.rb
+++ lib/ungulate/file_upload.rb
@@ -1,57 +1,58 @@
 require 'active_support/core_ext/class/attribute_accessors'
 require 'active_support/json'
+module Ungulate
+  class FileUpload
+    attr_reader(:acl, :bucket_url, :conditions,
+                :key, :policy, :success_action_redirect)
 
-class Ungulate::FileUpload
-  attr_reader(:acl, :bucket_url, :conditions,
-              :key, :policy, :success_action_redirect)
+    class << self
+      def enqueue(job_description)
+        queue.push(job_description.to_yaml)
+      end
 
-  class << self
-    def enqueue(job_description)
-      queue.push(job_description.to_yaml)
+      protected
+
+      def queue
+        @queue ||= Ungulate.configuration.queue.call
+      end
+    end
+
+    def initialize(options = {})
+      @bucket_url = options[:bucket_url]
+      @key = options[:key]
+      @policy = options[:policy]
+
+      if @policy
+        @policy['expiration'] = @policy['expiration'].utc
+
+        @conditions =
+          @policy['conditions'].map {|condition| condition.to_a.flatten }
+
+        @acl, @success_action_redirect =
+          Hash[@conditions].values_at('acl', 'success_action_redirect')
+
+        @policy =
+          Base64.encode64(ActiveSupport::JSON.encode(@policy)).gsub("\n", '')
+      end
+    end
+
+    def access_key_id
+      config.access_key_id
+    end
+
+    def signature
+      Base64.encode64(
+        OpenSSL::HMAC.digest(
+          OpenSSL::Digest::Digest.new('sha1'),
+          config.secret_access_key,
+          policy
+      )).gsub("\n", '')
     end
 
     protected
 
-    def queue
-      @queue ||= Ungulate.configuration.queue.call
+    def config
+      Ungulate.configuration
     end
-  end
-
-  def initialize(options = {})
-    @bucket_url = options[:bucket_url]
-    @key = options[:key]
-    @policy = options[:policy]
-
-    if @policy
-      @policy['expiration'] = @policy['expiration'].utc
-
-      @conditions =
-        @policy['conditions'].map {|condition| condition.to_a.flatten }
-
-      @acl, @success_action_redirect =
-        Hash[@conditions].values_at('acl', 'success_action_redirect')
-
-      @policy =
-        Base64.encode64(ActiveSupport::JSON.encode(@policy)).gsub("\n", '')
-    end
-  end
-
-  def access_key_id
-    config.access_key_id
-  end
-
-  def signature
-    Base64.encode64(
-      OpenSSL::HMAC.digest(
-        OpenSSL::Digest::Digest.new('sha1'),
-        config.secret_access_key,
-        policy
-    )).gsub("\n", '')
-  end
-
-  protected
-
-  def config
-    Ungulate.configuration
   end
 end
