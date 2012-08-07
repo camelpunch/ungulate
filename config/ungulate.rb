f233c103aba5a51235d2bc688088ec60795a8f35
Andrew Bruce <andrew@camelpunch.com>
2012-08-06 21:00:01 -0700

    Correct a comment in config/ungulate.rb
    The test_* config settings aren't purely used for cucumber

--- config/ungulate.rb
+++ config/ungulate.rb
@@ -19,7 +19,7 @@ Ungulate.configure do |config|
 
   config.server_sleep = 2 # seconds
 
-  # Cucumber settings
+  # Test settings
   # config.test_bucket = 'ungulate-test'
   # config.test_upload_key = 'some-file'
   # config.test_success_action_redirect_path = '/some-path'
