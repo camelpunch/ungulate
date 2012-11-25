6bcb3d4cb3c3f7e43bc0cf2b43cfc2f6f7a58683
Andrew Bruce <andrew@camelpunch.com>
2012-11-25 09:21:20 -0800

    update http spec
    I'd deleted the old test endpoint

--- spec/lib/ungulate/http_spec.rb
+++ spec/lib/ungulate/http_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 require 'ungulate/http'
 
 describe Ungulate::Http, :integration => true do
-  subject do
-    Ungulate::Http.new(:logger => ::Logger.new(nil))
-  end
+  subject(:http) { Ungulate::Http.new(:logger => ::Logger.new(nil)) }
 
   it "can return the body of a resource from https" do
-    subject.get_body('https://dmxno528jhfy0.cloudfront.net/superhug-watermark.png').
+    http.get_body('https://d1h33x9n430k4r.cloudfront.net/cute-sad-cat.jpg').
       should == fixture('watermark.png')
   end
 
   it "can PUT to a HTTP URL" do
-    response = subject.put('https://dmxno528jhfy0.cloudfront.net/superhug-watermark.png')
+    response = http.put('https://d1h33x9n430k4r.cloudfront.net/cute-sad-cat.jpg')
     response.code.should == 403
   end
 end
